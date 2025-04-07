52089bf87a9b78b73a0329f29e896efe4fb975d3
Geeteshgit <143749482+Geeteshgit@users.noreply.github.com>
2025-04-07 13:38:13 +0530

    final fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter> 
     </AuthProvider>
-  </GoogleOAuthProvider>
 )
