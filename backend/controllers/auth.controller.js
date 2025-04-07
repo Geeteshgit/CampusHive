28317f6430af86572f602bf37bd7d5a04fabcf74
Geeteshgit <143749482+Geeteshgit@users.noreply.github.com>
2025-04-07 23:49:32 +0530

    final fixes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -107,6 +107,7 @@ const getCurrentUser = async (req, res) => {
         if (!user) return res.status(200).json({ user: null });
 
         return res.status(200).json({ user });
+
     } catch (err) {
         return res.status(200).json({ user: null });
     }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { register, login, logout, getCurrentUser } = require("../controllers/auth.controller.js");
 const router = express.Router();
 
+
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
